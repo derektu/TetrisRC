4b677b27358ec69f08eb74cec427b0fc11aaef3c
derektu <derektu123@gmail.com>
2016-04-20 13:59:59 +0800

    - 建立 App/Components/Main.js, 提供主畫面元件. - 這樣子的話未來如果android/iOS都要支援的話 就可以使用共用的元件. - 未來的元件都會放在 App/Components底下, 讓主目錄的地方乾淨一些.

---
+++ App/Components/Main.js
@@ -0,0 +1,38 @@
+import React, {
+  Component,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+// 主畫面
+//
+class Main extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+module.exports = Main;
+
+
